d2e47522ee9f7a13095def8e9ae3bf2282f579ac
unknown <53521611+thelmaanjos@users.noreply.github.com>
2021-02-11 14:55:46 -0300

    Otimizacao da classe calculadora

--- src/main/java/br/edu/infnet/appsdi/tp2/CalculadoraTeste.java
+++ src/main/java/br/edu/infnet/appsdi/tp2/CalculadoraTeste.java
@@ -4,38 +4,89 @@ import java.util.Scanner;
 
 public class CalculadoraTeste {
 
+	private static float numeroA;
+	private static float numeroB;
+	private static final int SOMA = 1;
+	private static final int SUBTRACAO = 2;
+	private static final int DIVISAO = 3;
+	private static final int MULTIPLICACAO = 4;
+	
+	private static float soma() {
+		return numeroA + numeroB;
+	}
+	
+	private static float subtracao() {
+		return numeroA - numeroB;
+	}
+	
+	private static float multiplicacao() {
+		return numeroA * numeroB;
+	}
+	
+	private static float divisao() {
+		return numeroA / numeroB;
+	}
+	
+	private static float calcular(int oper) {
+		switch(oper) {
+		case SOMA:
+			return soma();
+		case SUBTRACAO:
+			return subtracao();
+		case DIVISAO:
+			return divisao();
+		case MULTIPLICACAO:
+			return multiplicacao();
+		}
+		return 0;
+	}
+	
+	private static int definirOperacao(String[] argumentos) {
+		
+		if(argumentos.length == 4) {
+			return Integer.valueOf(argumentos[3]);
+		}
+		else {
+			return tratarMenu();
+		}
+	}
+	
+	private static int tratarMenu() {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println(SOMA + " - soma");
+		System.out.println(SUBTRACAO + " - subtração");
+		System.out.println(DIVISAO + " - divisão");
+		System.out.println(MULTIPLICACAO + " - multiplicação");
+		System.out.println("Insira a operação a ser realizada:");		
+		
+		System.out.print("Selecione a opção desejada: ");
+		return input.nextInt();
+	}
+	
 		public static void main(String[] args) {
-			float numeroA = Float.valueOf(args[0]);
-			float numeroB = Float.valueOf(args[1]);
-			int operacao = Integer.valueOf(args[2]);
-			String nome = args[3];
-			float resultado = 0;
-
-			System.out.println("1 - soma");
-			System.out.println("2 - subtração");
-			System.out.println("3 - divisão");
-			System.out.println("4 - multiplicação");
-			System.out.println("Insira a operação a ser realizada:");
-						
-			if (operacao == 1) // TDD
+			numeroA = Float.valueOf(args[0]);
+			numeroB = Float.valueOf(args[1]);
+			String nome = args[2];
+			int operacao = definirOperacao(args);
+			float resultado = calcular(operacao);
+			
+			if (operacao == SOMA) // TDD
 			{ 
-				resultado = numeroA + numeroB;
 				System.out.printf("[%s] Soma: %.2f = %.2f + %.2f", nome, resultado, numeroA, numeroB);
 			} 
-			else if (operacao == 2) 
+			else if (operacao == SUBTRACAO) 
 			{
-				resultado = numeroA - numeroB;
-				System.out.printf("[%s] Subtração: %.2f = %.2f - %.2f", resultado, numeroA, numeroB);
+				System.out.printf("[%s] Subtração: %.2f = %.2f - %.2f", nome,resultado, numeroA, numeroB);
 			} 
-			else if (operacao == 3) 
+			else if (operacao == DIVISAO) 
 			{
-				resultado = numeroA / numeroB;
-				System.out.printf("[%s] Divisão: %.2f = %.2f / %.2f", resultado, numeroA, numeroB);
+				System.out.printf("[%s] Divisão: %.2f = %.2f / %.2f", nome, resultado, numeroA, numeroB);
 			} 
-			else if (operacao == 4) 
+			else if (operacao == MULTIPLICACAO) 
 			{
-				resultado = numeroA * numeroB;
-				System.out.printf("[%s] Multiplicação: %.2f = %.2f * %.2f", resultado, numeroA, numeroB);
+				System.out.printf("[%s] Multiplicação: %.2f = %.2f * %.2f", nome, resultado, numeroA, numeroB);
 			}
 			else 
 			{
